b452f5ad937e86a55a2443d4e012634be2fc964e
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-11-21 21:23:03 +0800

    feat: Add environment configuration and mongoose setup

--- app.js
+++ app.js
@@ -4,9 +4,32 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Environment Configuration
+const dotenv = require("dotenv");
+dotenv.config({path: "./.env"});
+
+// Mongoose
+const mongoose = require('mongoose');
+
+const DB = process.env.DB_PATH.replace(
+  "<DB_USERNAME>", process.env.DB_USERNAME
+).replace(
+  "<DB_PASSWORD>", process.env.DB_PASSWORD
+).replace(
+  "<DB_NAME>", process.env.DB_NAME
+)
+
+mongoose.connect(DB)
+  .then(
+    () => {console.log("success")},
+    error => {console.log("error", error.reason)}
+  );
+
+// Express App Initialization
 var app = express();
 
 // cors
