0b9b83ecee2626122c20346a8b45044bab556933
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-03 20:31:41 +0800

    chore: Add error handling with http-errors and update app.js
    - Installed `http-errors` using `npm install http-errors`.
    - Added a 404 error handler using `http-errors` to catch and forward unhandled routes.
    - Implemented a centralized error handler to process and render error responses.
    - Updated `app.js` to include `createError` from `http-errors`.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
 
 // Router
 var indexRouter = require('./routes/index');
@@ -47,5 +48,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
