6f1f2d6c0e46cc428c4b8acb276188ae14cb9071
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-04 21:20:02 +0800

    feat: Refactor database connection logic into a separate module
    - Moved the database connection setup from `app.js` to `connections/index.js` for improved code organization.

--- app.js
+++ app.js
@@ -11,26 +11,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 
-// Environment Configuration
-const dotenv = require("dotenv");
-dotenv.config({path: "./.env"});
-
-// Mongoose
-const mongoose = require('mongoose');
-
-const DB = process.env.DB_PATH.replace(
-  "<DB_USERNAME>", process.env.DB_USERNAME
-).replace(
-  "<DB_PASSWORD>", process.env.DB_PASSWORD
-).replace(
-  "<DB_NAME>", process.env.DB_NAME
-)
-
-mongoose.connect(DB)
-  .then(
-    () => {console.log("success")},
-    error => {console.log("error", error.reason)}
-  );
+// connections
+require("./connections");
 
 // Express App Initialization
 var app = express();
---
+++ connections/index.js
@@ -0,0 +1,22 @@
+// Environment Configuration
+const dotenv = require("dotenv");
+dotenv.config({path: "./.env"});
+
+// Mongoose
+const mongoose = require('mongoose');
+
+const DB = process.env.DB_PATH.replace(
+  "<DB_USERNAME>", process.env.DB_USERNAME
+).replace(
+  "<DB_PASSWORD>", process.env.DB_PASSWORD
+).replace(
+  "<DB_NAME>", process.env.DB_NAME
+)
+
+mongoose.connect(DB)
+  .then(
+    () => { console.log("success") },
+  )
+  .catch(
+    error => { console.log("error", error.reason) }
+  );
