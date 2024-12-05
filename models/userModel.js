b9a0dadcd0d311b8b0408643247114748a7f19c8
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-05 14:39:42 +0800

    feat: Update user model with additional fields and validations
    - Added `gender` field with an enum constraint (`male`, `female`) for better data structuring.
    - Introduced `password` field with validation:
      - Minimum length of 8 characters.
      - Excluded from default query results (`select: false`).
    - Set a default value for the `photo` field to avoid null entries.
    - Enhanced schema to better align with authentication requirements and user data handling.

--- models/userModel.js
+++ models/userModel.js
@@ -4,16 +4,29 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "A name is required to register."]
+      required: [true, "A name is required."]
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"]
     },
     email: {
       type: String,
-      required: [true, "An email is required to register"],
+      required: [true, "An email is required."],
       unique: true,
       lowercase: true,
       select: false
     },
-    photo: String
+    password: {
+      type: String,
+      required: [true, "An password is required."],
+      minlength: 8,
+      select: false
+    },
+    photo: {
+      type: String,
+      default: ""
+    }
   },
   {
     versionKey: false,
